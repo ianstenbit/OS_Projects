1cb10e65f3ebdebb9bd1ce775e9d9933941145f6
Ian Johnson <ianjjohnson@icloud.com>
2017-04-06 12:09:47 -0500

    final tweaks

--- ProgrammingAssignment4/main.c
+++ ProgrammingAssignment4/main.c
@@ -1,4 +1,5 @@
 #define PAGE_REF_STRING_LENGTH 20
+#define DEBUG 0 //Set to 1 for more informations
 
 #include <stdlib.h>
 #include <string.h>
@@ -92,38 +93,46 @@ void algorithms(int* page_ref, int max_frames){
     //FIFO
     if(!contains(FIFO_memory, page_ref[i], max_frames)){
 
-      printf("Page fault in FIFO on page number %i at index %i\n", page_ref[i], i);
-      printf("\nMemory before swap: [");
-      for(int j = 0; j < i && j < max_frames; j++) printf("%i,", FIFO_memory[j]);
-      printf("\b]\n");
+      if(DEBUG){
+        printf("Page fault in FIFO on page number %i at index %i\n", page_ref[i], i);
+        printf("\tMemory before swap: [");
+        for(int j = 0; j < max_frames; j++) printf("%i,", (FIFO_memory[j] == 16843009)? -1 : FIFO_memory[j]);
+        printf("\b]\n");
+      }
 
       FIFO_faults++;
       int repl_index = arg_min(entered, max_frames);
       FIFO_memory[repl_index] = page_ref[i];
       entered[repl_index] = i + 1;
 
-      printf("\nMemory after swap: [");
-      for(int j = 0; j < i && j < max_frames; j++) printf("%i,", FIFO_memory[j]);
-      printf("\b]\n");
+      if(DEBUG){
+        printf("\tMemory after swap: [");
+        for(int j = 0; j < max_frames; j++) printf("%i,", (FIFO_memory[j] == 16843009)? -1 : FIFO_memory[j]);
+        printf("\b]\n");
+      }
 
     }
 
     //LRU
     if(!contains(LRU_memory, page_ref[i], max_frames)){
 
-      printf("Page fault in LRU on page number %i at index %i\n", page_ref[i], i);
-      printf("\nMemory before swap: [");
-      for(int j = 0; j < i && j < max_frames; j++) printf("%i,", LRU_memory[j]);
-      printf("\b]\n");
+      if(DEBUG){
+        printf("Page fault in LRU on page number %i at index %i\n", page_ref[i], i);
+        printf("\tMemory before swap: [");
+        for(int j = 0; j < max_frames; j++) printf("%i,", (LRU_memory[j] == 16843009)? -1 : LRU_memory[j]);
+        printf("\b]\n");
+      }
 
       LRU_faults++;
       int repl_index = arg_min(used, max_frames);
       LRU_memory[repl_index] = page_ref[i];
       used[repl_index] = 0;
 
-      printf("\nMemory after swap: [");
-      for(int j = 0; j < i && j < max_frames; j++) printf("%i,", LRU_memory[j]);
-      printf("\b]\n");
+      if(DEBUG){
+        printf("\tMemory after swap: [");
+        for(int j = 0; j < max_frames; j++) printf("%i,", (LRU_memory[j] == 16843009)? -1 : LRU_memory[j]);
+        printf("\b]\n");
+      }
 
     }
 
@@ -132,24 +141,29 @@ void algorithms(int* page_ref, int max_frames){
     //OPT
     if(!contains(OPT_memory, page_ref[i], max_frames)){
 
-      printf("Page fault in OPT on page number %i at index %i\n", page_ref[i], i);
-      printf("\nMemory before swap: [");
-      for(int j = 0; j < i && j < max_frames; j++) printf("%i,", OPT_memory[j]);
-      printf("\b]\n");
+      if(DEBUG){
+        printf("Page fault in OPT on page number %i at index %i\n", page_ref[i], i);
+        printf("\tMemory before swap: [");
+        for(int j = 0; j < max_frames; j++) printf("%i,", (OPT_memory[j] == 16843009)? -1 : OPT_memory[j]);
+        printf("\b]\n");
+      }
 
       OPT_faults++;
       int repl_index = OPT_helper(OPT_memory, page_ref, i, max_frames);
       OPT_memory[repl_index] = page_ref[i];
 
-      printf("\nMemory after swap: [");
-      for(int j = 0; j < i && j < max_frames; j++) printf("%i,", OPT_memory[j]);
-      printf("\b]\n");
+      if(DEBUG){
+        printf("\tMemory after swap: [");
+        for(int j = 0; j < max_frames; j++) printf("%i,", (OPT_memory[j] == 16843009)? -1 : OPT_memory[j]);
+        printf("\b]\n");
+      }
 
     }
 
 
   }
 
+  printf("Number of Page Frames: %i\n", max_frames);
   printf("\tFIFO Faults: %i\n", FIFO_faults);
   printf("\tLRU Faults: %i\n", LRU_faults);
   printf("\tOPT Faults: %i\n", OPT_faults);
@@ -176,9 +190,6 @@ int main(int argc, char* argv[]){
     int population_of_page_frames = 0;
 
     for(int max_pages = 1; max_pages <= 7; max_pages++){
-
-        printf("Number of Page Frames: %i\n", max_pages);
-
         //printf("\tFIFO: %i faults\n", FIFO(page_ref_string, max_pages, memory));
         //printf("\tLRU: %i faults\n", LRU(page_ref_string, max_pages, memory));
         //OPT(page_ref_string, max_pages, memory)
